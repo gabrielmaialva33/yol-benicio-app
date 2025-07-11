5d8048db995dde59e622beb471d59f2885adc0e9
Gabriel Maia <gabrielmaialva33@gmail.com>
2025-07-11 16:09:48 -0300

    chore: integrate CocoaPods into macOS project and rename `YolLogo` to `BenicioLogo` for branding consistency

--- lib/features/common/widgets/benicio_logo.dart
+++ lib/features/common/widgets/benicio_logo.dart
@@ -4,11 +4,11 @@ class BenicioLogo extends StatelessWidget {
   final double? width;
   final double? height;
 
-  const YolLogo({
-    Key? key,
+  const BenicioLogo({
+    super.key,
     this.width,
     this.height,
-  }) : super(key: key);
+  });
 
   @override
   Widget build(BuildContext context) {
