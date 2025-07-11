44ead6cf7c8a09fdabb465ff4bbd3e985f8c2c15
Gabriel Maia <gabrielmaialva33@gmail.com>
2025-07-11 18:00:01 -0300

    chore: remove unused `AppTextStyles` import from `ProfilePage` to clean up dependencies

--- lib/features/profile/profile_page.dart
+++ lib/features/profile/profile_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../../core/theme/theme_provider.dart';
-import '../../core/theme/app_text_styles.dart';
 import '../shared/services/auth_service.dart';
 
 class ProfilePage extends StatefulWidget {
