2ddcf7b7dbe51fed803ef9c393ee3dc540b86001
Gabriel Maia <gabrielmaialva33@gmail.com>
2025-07-11 15:48:38 -0300

    chore: update widget test import path for Benicio package rename

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:yolapp/main.dart';
+import 'package:benicio/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
